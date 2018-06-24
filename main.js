234e5cab4b00109036c4e76c8d622458fca5d269
ykmichelle <ykmichelle@gmail.com>
2018-06-24 19:16:23 +0800

    updated

--- main.js
+++ main.js
@@ -10,6 +10,3 @@ http.createServer(function (request, response) {
    // Send the response body as "Hello World"
    response.end('Hello World\n');
 }).listen(80);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
